0d16f14702d97993a2459284e0f9846c1e9a4656
Abdelhamid El Bouni <elbouni2003@gmail.com>
2024-09-29 03:18:18 +0200

    uppload server folder

---
+++ server/db/index.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb+srv://elbouni2003:mongo12db34@cluster0.bbe3w.mongodb.net/")
+.then(()=>console.log("conected"))
+.catch((e)=>console.log(e))
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("./db/index")
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.use("/api",(req,res)=>{
+    res.send("hello api")
+})
+app.listen(5000,()=> console.log("am listening in port 5000"))
