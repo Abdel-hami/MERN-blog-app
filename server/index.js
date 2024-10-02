cf25c3e26856a90b81677d0a6c4fa7eac622ba68
Abdelhamid El Bouni <elbouni2003@gmail.com>
2024-10-02 17:12:27 +0200

    backen endeed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const blogRouter = require("./route/route")
+
+
 require("./db/index")
+
+
 const app = express(); 
 app.use(cors());
 app.use(express.json())
+
+app.use("/api/blogs", blogRouter)
 
 app.use("/api",(req,res)=>{
     res.send("hello api")
